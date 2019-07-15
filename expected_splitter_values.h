1e426e0766757c7e0598aae5ca90a2f991596d98
Bradley A Smith <bradley@baysmith.com>
2019-07-15 01:14:04 -0700

    Tests check values rather than just print

---
+++ expected_splitter_values.h
@@ -0,0 +1,54 @@
+float expected_splitter_values[][12] = {{
+                                            -10, 6, -4,     // splitter
+                                            -266, 256, -10, // left_child
+                                            -10, 6, -4,     // splitter_bar
+                                            -4, 1, -3,      // right_child
+                                        },
+                                        {
+                                            0, 6, 6,      // splitter
+                                            -256, 256, 0, // left_child
+                                            0, 6, 6,      // splitter_bar
+                                            6, 1, 7,      // right_child
+                                        },
+                                        {
+                                            10, 6, 16,     // splitter
+                                            -246, 256, 10, // left_child
+                                            10, 6, 16,     // splitter_bar
+                                            16, 1, 17,     // right_child
+                                        },
+                                        {
+                                            20, 6, 26,     // splitter
+                                            -236, 256, 20, // left_child
+                                            20, 6, 26,     // splitter_bar
+                                            26, 1, 27,     // right_child
+                                        },
+                                        {
+                                            30, 6, 36,     // splitter
+                                            -226, 256, 30, // left_child
+                                            30, 6, 36,     // splitter_bar
+                                            36, 1, 37,     // right_child
+                                        },
+                                        {
+                                            40, 6, 46,     // splitter
+                                            -216, 256, 40, // left_child
+                                            40, 6, 46,     // splitter_bar
+                                            46, 1, 47,     // right_child
+                                        },
+                                        {
+                                            60, 6, 66,     // splitter
+                                            -196, 256, 60, // left_child
+                                            60, 6, 66,     // splitter_bar
+                                            66, 1, 67,     // right_child
+                                        },
+                                        {
+                                            70, 6, 76,     // splitter
+                                            -186, 256, 70, // left_child
+                                            70, 6, 76,     // splitter_bar
+                                            76, 1, 77,     // right_child
+                                        },
+                                        {
+                                            80, 6, 86,     // splitter
+                                            -176, 256, 80, // left_child
+                                            80, 6, 86,     // splitter_bar
+                                            86, 1, 87,     // right_child
+                                        }};
--- test.c
+++ test.c
@@ -77,6 +77,8 @@ static void am_dumprow(am_Row *row)
 
 static void am_dumpsolver(am_Solver *solver)
 {
+    if (!solver)
+        return;
     am_Row *row = NULL;
     int idx = 0;
     printf("-------------------------------\n");
@@ -121,7 +123,7 @@ static am_Constraint *new_constraint(am_Solver *in_solver, double in_strength,
     return c;
 }
 
-static void test_all(void)
+static void test_all()
 {
     printf("test_all...\n");
     am_Solver *solver;
@@ -460,14 +462,13 @@ static void test_binarytree()
     printf("test_binarytree passed\n");
 }
 
-static void test_unbounded(void)
+static void test_unbounded()
 {
+    printf("test_unbounded...\n");
     am_Solver *solver;
     am_Variable *x, *y;
     am_Constraint *c;
     int ret = setjmp(jbuf);
-    printf("\n\n==========\ntest unbound\n");
-    printf("ret = %d\n", ret);
     if (ret < 0) {
         perror("setjmp");
         return;
@@ -486,18 +487,16 @@ static void test_unbounded(void)
     am_addconstant(c, 10.0);
     am_setrelation(c, AM_EQUAL);
     ret = am_add(c);
-    printf("ret = %d\n", ret);
     assert(ret == AM_UNSATISFIED);
-    am_dumpsolver(solver);
+    assert(solver->rows.count == 0);
 
     /* 0.0 == 0.0 */
     c = am_newconstraint(solver, AM_REQUIRED);
     am_addconstant(c, 0.0);
     am_setrelation(c, AM_EQUAL);
     ret = am_add(c);
-    printf("ret = %d\n", ret);
     assert(ret == AM_OK);
-    am_dumpsolver(solver);
+    assert(solver->rows.count == 1);
 
     am_resetsolver(solver, 1);
 
@@ -507,9 +506,10 @@ static void test_unbounded(void)
     am_setrelation(c, AM_GREATEQUAL);
     am_addconstant(c, 10.0);
     ret = am_add(c);
-    printf("ret = %d\n", ret);
     assert(ret == AM_OK);
-    am_dumpsolver(solver);
+    assert(solver->rows.count == 1);
+    am_updatevars(solver);
+    assert(am_value(x) == 10.0);
 
     /* x == 2*y */
     c = am_newconstraint(solver, AM_REQUIRED);
@@ -517,9 +517,11 @@ static void test_unbounded(void)
     am_setrelation(c, AM_EQUAL);
     am_addterm(c, y, 2.0);
     ret = am_add(c);
-    printf("ret = %d\n", ret);
     assert(ret == AM_OK);
-    am_dumpsolver(solver);
+    assert(solver->rows.count == 2);
+    am_updatevars(solver);
+    assert(am_value(x) == 10.0);
+    assert(am_value(y) == 5.0);
 
     /* y == 3*x */
     c = am_newconstraint(solver, AM_REQUIRED);
@@ -527,11 +529,12 @@ static void test_unbounded(void)
     am_setrelation(c, AM_EQUAL);
     am_addterm(c, x, 3.0);
     ret = am_add(c);
-    printf("ret = %d\n", ret);
     assert(ret == AM_UNBOUND);
-    am_dumpsolver(solver);
+    assert(solver->rows.count == 2);
+    am_updatevars(solver);
 
     am_resetsolver(solver, 1);
+    assert(solver->rows.count == 0);
 
     /* x >= 10.0 */
     c = am_newconstraint(solver, AM_REQUIRED);
@@ -539,22 +542,21 @@ static void test_unbounded(void)
     am_setrelation(c, AM_GREATEQUAL);
     am_addconstant(c, 10.0);
     ret = am_add(c);
-    printf("ret = %d\n", ret);
+    assert(solver->rows.count == 1);
     assert(ret == AM_OK);
-    am_dumpsolver(solver);
+    am_updatevars(solver);
+    assert(am_value(x) == 10.0);
 
     /* x <= 0.0 */
     c = am_newconstraint(solver, AM_REQUIRED);
     am_addterm(c, x, 1.0);
     am_setrelation(c, AM_LESSEQUAL);
     ret = am_add(c);
-    printf("ret = %d\n", ret);
+    assert(solver->rows.count == 1);
     assert(ret == AM_UNBOUND);
-    am_dumpsolver(solver);
-
-    printf("x: %f\n", am_value(x));
 
     am_resetsolver(solver, 1);
+    assert(solver->rows.count == 0);
 
     /* x == 10.0 */
     c = am_newconstraint(solver, AM_REQUIRED);
@@ -562,9 +564,8 @@ static void test_unbounded(void)
     am_setrelation(c, AM_EQUAL);
     am_addconstant(c, 10.0);
     ret = am_add(c);
-    printf("ret = %d\n", ret);
+    assert(solver->rows.count == 1);
     assert(ret == AM_OK);
-    am_dumpsolver(solver);
 
     /* x == 20.0 */
     c = am_newconstraint(solver, AM_REQUIRED);
@@ -572,9 +573,8 @@ static void test_unbounded(void)
     am_setrelation(c, AM_EQUAL);
     am_addconstant(c, 20.0);
     ret = am_add(c);
-    printf("ret = %d\n", ret);
     assert(ret == AM_UNSATISFIED);
-    am_dumpsolver(solver);
+    assert(solver->rows.count == 1);
 
     /* x == 10.0 */
     c = am_newconstraint(solver, AM_REQUIRED);
@@ -582,25 +582,23 @@ static void test_unbounded(void)
     am_setrelation(c, AM_EQUAL);
     am_addconstant(c, 10.0);
     ret = am_add(c);
-    printf("ret = %d\n", ret);
     assert(ret == AM_OK);
-    am_dumpsolver(solver);
+    assert(solver->rows.count == 2);
 
     am_delsolver(solver);
-    printf("allmem = %d\n", (int)allmem);
-    printf("maxmem = %d\n", (int)maxmem);
+    assert(maxmem == 9680);
     assert(allmem == 0);
     maxmem = 0;
+    printf("test_unbounded passed\n");
 }
 
-static void test_strength(void)
+static void test_strength()
 {
+    printf("test_strength...\n");
     am_Solver *solver;
     am_Variable *x, *y;
     am_Constraint *c;
     int ret = setjmp(jbuf);
-    printf("\n\n==========\ntest strength\n");
-    printf("ret = %d\n", ret);
     if (ret < 0) {
         perror("setjmp");
         return;
@@ -619,41 +617,35 @@ static void test_strength(void)
     new_constraint(solver, AM_STRONG, x, 1.0, AM_LESSEQUAL, 0.0, y, 1.0, END);
     new_constraint(solver, AM_MEDIUM, x, 1.0, AM_EQUAL, 50, END);
     c = new_constraint(solver, AM_MEDIUM - 10, y, 1.0, AM_EQUAL, 40, END);
-    printf("%f, %f\n", am_value(x), am_value(y));
     assert(am_value(x) == 50);
     assert(am_value(y) == 50);
 
     am_setstrength(c, AM_MEDIUM + 10);
-    printf("%f, %f\n", am_value(x), am_value(y));
     assert(am_value(x) == 40);
     assert(am_value(y) == 40);
 
     am_setstrength(c, AM_MEDIUM - 10);
-    printf("%f, %f\n", am_value(x), am_value(y));
     assert(am_value(x) == 50);
     assert(am_value(y) == 50);
 
     am_delsolver(solver);
-    printf("allmem = %d\n", (int)allmem);
-    printf("maxmem = %d\n", (int)maxmem);
+    assert(maxmem == 9616);
     assert(allmem == 0);
     maxmem = 0;
+    printf("test_strength passed\n");
 }
 
-static void test_suggest(void)
+static void test_suggest()
 {
+    printf("test_suggest...\n");
 #if 1
-    /* This should be valid but fails the (enter.id != 0) assertion in
-     * am_dual_optimize() */
     am_Float strength1 = AM_REQUIRED;
     am_Float strength2 = AM_REQUIRED;
-    am_Float width = 76;
+    size_t expected_maxmem = 12048;
 #else
-    /* This mostly works, but still insists on forcing left_child_l = 0 which it
-     * should not */
     am_Float strength1 = AM_STRONG;
     am_Float strength2 = AM_WEAK;
-    am_Float width = 76;
+    size_t expected_maxmem = 12112;
 #endif
     am_Float delta = 0;
     am_Float pos;
@@ -663,8 +655,6 @@ static void test_suggest(void)
     am_Variable *splitter_bar_l, *splitter_bar_w, *splitter_bar_r;
     am_Variable *right_child_l, *right_child_w, *right_child_r;
     int ret = setjmp(jbuf);
-    printf("\n\n==========\ntest suggest\n");
-    printf("ret = %d\n", ret);
     if (ret < 0) {
         perror("setjmp");
         return;
@@ -723,55 +713,80 @@ static void test_suggest(void)
                    splitter_r, 1.0, END);
     new_constraint(solver, strength2, left_child_w, 1.0, AM_EQUAL, 256.0, END);
 
-    /* splitter_l = 0 */
-    /* splitter_r = 76 */
-    new_constraint(solver, AM_REQUIRED, splitter_l, 1.0, AM_EQUAL, 0.0, END);
-    new_constraint(solver, AM_REQUIRED, splitter_r, 1.0, AM_EQUAL, width, END);
-
-    printf("\n\n==========\ntest suggest\n");
-    for (pos = -10; pos < 56; pos++) {
+#include "expected_splitter_values.h"
+    int exptected_index = 0;
+    for (pos = -10; pos < 50; pos += 10) {
         am_suggest(splitter_bar_l, pos);
-        printf("pos: %4g | ", pos);
-        printf("splitter_l l=%2g, w=%2g, r=%2g | ", am_value(splitter_l),
-               am_value(splitter_w), am_value(splitter_r));
-        printf("left_child_l l=%2g, w=%2g, r=%2g | ", am_value(left_child_l),
-               am_value(left_child_w), am_value(left_child_r));
-        printf("splitter_bar_l l=%2g, w=%2g, r=%2g | ",
-               am_value(splitter_bar_l), am_value(splitter_bar_w),
-               am_value(splitter_bar_r));
-        printf("right_child_l l=%2g, w=%2g, r=%2g | ", am_value(right_child_l),
-               am_value(right_child_w), am_value(right_child_r));
-        printf("\n");
+        am_updatevars(solver);
+        assert(am_value(splitter_l) ==
+               expected_splitter_values[exptected_index][0]);
+        assert(am_value(splitter_w) ==
+               expected_splitter_values[exptected_index][1]);
+        assert(am_value(splitter_r) ==
+               expected_splitter_values[exptected_index][2]);
+        assert(am_value(left_child_l) ==
+               expected_splitter_values[exptected_index][3]);
+        assert(am_value(left_child_w) ==
+               expected_splitter_values[exptected_index][4]);
+        assert(am_value(left_child_r) ==
+               expected_splitter_values[exptected_index][5]);
+        assert(am_value(splitter_bar_l) ==
+               expected_splitter_values[exptected_index][6]);
+        assert(am_value(splitter_bar_w) ==
+               expected_splitter_values[exptected_index][7]);
+        assert(am_value(splitter_bar_r) ==
+               expected_splitter_values[exptected_index][8]);
+        assert(am_value(right_child_l) ==
+               expected_splitter_values[exptected_index][9]);
+        assert(am_value(right_child_w) ==
+               expected_splitter_values[exptected_index][10]);
+        assert(am_value(right_child_r) ==
+               expected_splitter_values[exptected_index][11]);
+        ++exptected_index;
     }
 
     am_autoupdate(solver, 1);
 
-    printf("\n\n==========\ntest suggest\n");
-    for (pos = 57; pos < 86; pos++) {
+    for (pos = 60; pos < 86; pos += 10) {
         am_suggest(splitter_bar_l, pos);
-        printf("pos: %4g | ", pos);
-        printf("splitter_l l=%2g, w=%2g, r=%2g | ", am_value(splitter_l),
-               am_value(splitter_w), am_value(splitter_r));
-        printf("left_child_l l=%2g, w=%2g, r=%2g | ", am_value(left_child_l),
-               am_value(left_child_w), am_value(left_child_r));
-        printf("splitter_bar_l l=%2g, w=%2g, r=%2g | ",
-               am_value(splitter_bar_l), am_value(splitter_bar_w),
-               am_value(splitter_bar_r));
-        printf("right_child_l l=%2g, w=%2g, r=%2g | ", am_value(right_child_l),
-               am_value(right_child_w), am_value(right_child_r));
-        printf("\n");
+        assert(am_value(splitter_l) ==
+               expected_splitter_values[exptected_index][0]);
+        assert(am_value(splitter_w) ==
+               expected_splitter_values[exptected_index][1]);
+        assert(am_value(splitter_r) ==
+               expected_splitter_values[exptected_index][2]);
+        assert(am_value(left_child_l) ==
+               expected_splitter_values[exptected_index][3]);
+        assert(am_value(left_child_w) ==
+               expected_splitter_values[exptected_index][4]);
+        assert(am_value(left_child_r) ==
+               expected_splitter_values[exptected_index][5]);
+        assert(am_value(splitter_bar_l) ==
+               expected_splitter_values[exptected_index][6]);
+        assert(am_value(splitter_bar_w) ==
+               expected_splitter_values[exptected_index][7]);
+        assert(am_value(splitter_bar_r) ==
+               expected_splitter_values[exptected_index][8]);
+        assert(am_value(right_child_l) ==
+               expected_splitter_values[exptected_index][9]);
+        assert(am_value(right_child_w) ==
+               expected_splitter_values[exptected_index][10]);
+        assert(am_value(right_child_r) ==
+               expected_splitter_values[exptected_index][11]);
+        ++exptected_index;
     }
 
     am_delsolver(solver);
-    printf("allmem = %d\n", (int)allmem);
-    printf("maxmem = %d\n", (int)maxmem);
     assert(allmem == 0);
+    assert(maxmem == expected_maxmem);
     maxmem = 0;
+    printf("test_suggest passed\n");
 }
 
 void test_cycling()
 {
-    am_Solver *solver = am_newsolver(NULL, NULL);
+    printf("test_cycling...\n");
+    am_Solver *solver = am_newsolver(debug_allocf, NULL);
 
     am_Variable *va = am_newvariable(solver);
     am_Variable *vb = am_newvariable(solver);
@@ -779,8 +794,7 @@ void test_cycling()
     am_Variable *vd = am_newvariable(solver);
 
     am_addedit(va, AM_STRONG);
-    printf("after edit\n");
-    am_dumpsolver(solver);
+    assert(solver->rows.count == 1);
 
     /* vb == va */
     {
@@ -791,7 +805,11 @@ void test_cycling()
         ret |= am_addterm(c, va, 1.0);
         ret |= am_add(c);
         assert(ret == AM_OK);
-        am_dumpsolver(solver);
+        assert(solver->rows.count == 2);
+        am_suggest(va, 1.5);
+        am_updatevars(solver);
+        assert(am_value(va) == 1.5);
+        assert(am_value(vb) == 1.5);
     }
 
     /* vb == vc */
@@ -803,7 +821,10 @@ void test_cycling()
         ret |= am_addterm(c, vc, 1.0);
         ret |= am_add(c);
         assert(ret == AM_OK);
-        am_dumpsolver(solver);
+        assert(solver->rows.count == 3);
+        am_updatevars(solver);
+        assert(am_value(vb) == 1.5);
+        assert(am_value(vc) == 1.5);
     }
 
     /* vc == vd */
@@ -815,7 +836,10 @@ void test_cycling()
         ret |= am_addterm(c, vd, 1.0);
         ret |= am_add(c);
         assert(ret == AM_OK);
-        am_dumpsolver(solver);
+        assert(solver->rows.count == 4);
+        am_updatevars(solver);
+        assert(am_value(vb) == 1.5);
+        assert(am_value(vd) == 1.5);
     }
 
     /* vd == va */
@@ -826,9 +850,18 @@ void test_cycling()
         ret |= am_setrelation(c, AM_EQUAL);
         ret |= am_addterm(c, va, 1.0);
         ret |= am_add(c);
-        assert(ret == AM_OK); /* asserts here */
-        am_dumpsolver(solver);
+        assert(ret == AM_OK);
+        assert(solver->rows.count == 5);
+        am_updatevars(solver);
+        assert(am_value(vd) == 1.5);
+        assert(am_value(va) == 1.5);
     }
+
+    am_delsolver(solver);
+    assert(allmem == 0);
+    assert(maxmem == 10320);
+    maxmem = 0;
+    printf("test_cycling passed\n");
 }
 
 void test_set_strength()
@@ -879,15 +912,17 @@ void test_null()
     printf("test_null passed\n");
 }
 
-int main(void)
+int main()
 {
-    test_unbounded();
-    test_strength();
-    test_suggest();
-    test_cycling();
+    test_all();
     test_null();
     test_set_strength();
+    test_cycling();
+    test_suggest();
     test_binarytree();
-    test_all();
+    test_strength();
+    test_unbounded();
+
+    (void)am_dumpsolver(NULL);
     return 0;
 }
